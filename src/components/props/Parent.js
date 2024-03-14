c5bef15c89f1cc1a732fc6b4a73a0ba2085e869b
jkannan10 <kannajay004@gmail.com>
2024-03-14 12:23:32 +0530

    first commit

---
+++ src/components/props/Parent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Child from "./Child";
+
+function Parent() {
+  return (
+    <div>
+      <Child name="xyz" city="abc" />
+    </div>
+  );
+}
+
+export default Parent;
